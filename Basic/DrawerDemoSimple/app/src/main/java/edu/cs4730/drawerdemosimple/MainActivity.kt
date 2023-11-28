72504efe02b8123a94ccd96e268e96d3bb2e171c
Jim Ward <JimSeker@users.noreply.github.com>
2023-11-27 20:48:11 -0700

    it's a working example now.

--- Basic/DrawerDemoSimple/app/src/main/java/edu/cs4730/drawerdemosimple/MainActivity.kt
+++ Basic/DrawerDemoSimple/app/src/main/java/edu/cs4730/drawerdemosimple/MainActivity.kt
@@ -3,15 +3,16 @@ package edu.cs4730.drawerdemosimple
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -20,19 +21,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import edu.cs4730.drawerdemosimple.ui.theme.DrawerDemoSimpleTheme
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,28 +57,42 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
+fun TopBar(drawerState: DrawerState) {
+    val coroutineScope = rememberCoroutineScope()
     TopAppBar(
         title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
-        colors = TopAppBarDefaults.smallTopAppBarColors(
+        colors = topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.primary
         ),
-
-        )
+        navigationIcon = {
+            IconButton(onClick = {
+                coroutineScope.launch {
+                    // opens drawer
+                    drawerState.open()
+                }
+            }) {
+                Icon(
+                    // internal hamburger menu
+                    Icons.Rounded.Menu,
+                    contentDescription = "MenuButton"
+                )
+            }
+        },
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TopBarPreview() {
-    TopBar()
+    TopBar(DrawerState(initialValue =  DrawerValue.Closed))
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+    var which by remember { mutableIntStateOf(1) }
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
@@ -82,28 +100,54 @@ fun MainScreen() {
                 Text("Drawer title", modifier = Modifier.padding(16.dp))
                 Divider()
                 NavigationDrawerItem(
-                    label = { Text(text = "Drawer Item") },
-                    selected = false,
-                    onClick = { /*TODO*/ }
+                    label = { Text(text = "First item") },
+                    selected = which == 1,
+                    onClick = {
+                        which = 1
+                        scope.launch { drawerState.close() }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Second item") },
+                    selected = which == 2,
+                    onClick = {
+                        which = 2
+                        scope.launch { drawerState.close() }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Third item") },
+                    selected = which == 3,
+                    onClick = {
+                        which = 3
+                        scope.launch { drawerState.close() }
+                    }
                 )
                 // ...other drawer items
             }
-
         },
-    ) {
+        gesturesEnabled = true,
+    ) { //content = is this part here.
         Scaffold(
-            topBar = { TopBar() },
-
+            topBar = { TopBar(drawerState)
+                     },
             content = { padding -> // We have to pass the scaffold inner padding to our content. That's why we use Box.
                 Text(
                     modifier = Modifier.padding(padding),
                     text = "Hi"
                 )
+                //top open the drawer, call this:
+                // scope.launch { drawerState.open() }
+                when (which) {
+                    1 -> FirstScreen()
+                    2 -> SecondScreen()
+                    3 -> ThirdScreen()
+                    else -> FirstScreen()
+                }
             },
         )
     }
 }
-
 
 
 @Preview(showBackground = true)
