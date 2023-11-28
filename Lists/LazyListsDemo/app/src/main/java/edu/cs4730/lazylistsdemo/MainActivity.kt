85fe1651f7ca3be9171531a6a5a73720c549a9c4
Jim Ward <JimSeker@users.noreply.github.com>
2023-11-28 14:45:47 -0700

    almost working list example.

---
+++ Lists/LazyListsDemo/app/src/main/java/edu/cs4730/lazylistsdemo/MainActivity.kt
@@ -0,0 +1,151 @@
+package edu.cs4730.lazylistsdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.cs4730.lazylistsdemo.ui.theme.LazyListsDemoTheme
+import java.util.Arrays
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyListsDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        ),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainScreen() {
+    val values = Arrays.asList(
+        "Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7",
+        "Max OS X", "Linux", "OS/2"
+    )
+    Scaffold(
+        topBar = {
+            TopBar()
+        },
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier.padding(padding)
+                ,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            item {
+                Text("Header")
+
+            }
+            itemsIndexed(values) { index, item ->
+                myItem(item, index)
+            }
+            item {
+                Text("Footer")
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
+
+
+@Composable
+fun myItem(value: String, index: Int) {
+    Box (
+        modifier = with (Modifier){
+            height(100.dp)
+               // .fillMaxWidth()
+                .paint(
+                    // Replace with your image id
+                    painterResource(id = R.drawable.phone),
+
+                    colorFilter =  ColorFilter.tint(Color.Gray, blendMode= BlendMode.Darken),
+                    contentScale = ContentScale.FillWidth)
+
+        })
+    {
+        // Add more views here!
+        Text(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            color= Color.White,
+            text = value)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun myItemPreview() {
+   myItem("Android", 1)
+}
