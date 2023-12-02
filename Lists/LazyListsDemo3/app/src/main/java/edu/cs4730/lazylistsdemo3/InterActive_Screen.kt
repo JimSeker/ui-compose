a738dcfd698ed9456a240ab28413880c84088354
Jim Ward <JimSeker@users.noreply.github.com>
2023-12-02 15:07:30 -0700

    checkboxes now update correctly.

--- Lists/LazyListsDemo3/app/src/main/java/edu/cs4730/lazylistsdemo3/InterActive_Screen.kt
+++ Lists/LazyListsDemo3/app/src/main/java/edu/cs4730/lazylistsdemo3/InterActive_Screen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,7 +27,9 @@ import androidx.compose.ui.unit.dp
 import java.util.ArrayList
 
 /**
- * this example displays, but the checkbox is updated it won't redraw.  idk why.... painful.
+ * this example displays, with checkboxes that change other parts of the list.
+ * this is kind of a painful example with all the remember as arrays, but I couldn't
+ * think of better way to do it.  And I'm trying to replicate the lists from recyclerviews. 
  */
 
 @Composable
@@ -34,26 +37,24 @@ fun InterActiveScreen() {
     val state = rememberLazyListState()
 
     val list = remember { mutableStateListOf<InterActive_DataModel>() }
-    list.add(InterActive_DataModel(0,"Linux"))
-    list.add(InterActive_DataModel(1,"Windows7"))
-    list.add(InterActive_DataModel(2,"Suse"))
-    list.add(InterActive_DataModel(3,"Eclipse"))
-    list.add(InterActive_DataModel(4,"Ubuntu"))
-    list.add(InterActive_DataModel(5,"Solaris"))
-    list.add(InterActive_DataModel(6,"Android"))
-    list.add(InterActive_DataModel(7,"iPhone"))
+    list.add(InterActive_DataModel(0, "Linux"))
+    list.add(InterActive_DataModel(1, "Windows7"))
+    list.add(InterActive_DataModel(2, "Suse"))
+    list.add(InterActive_DataModel(3, "Eclipse"))
+    list.add(InterActive_DataModel(4, "Ubuntu"))
+    list.add(InterActive_DataModel(5, "Solaris"))
+    list.add(InterActive_DataModel(6, "Android"))
+    list.add(InterActive_DataModel(7, "iPhone"))
     // Initially select one of the items
     list[1].FUSelected(true)
+    val checkedBoxes = List(list.size) { remember { mutableStateOf(false) } }
+    checkedBoxes[1].value = true
 
     LazyColumn(
         state = state,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-//        items(
-//            items = list,
-//            key = { entry -> entry.id }
-//        ) {entry ->
-            itemsIndexed(list) { index, entry ->
+        itemsIndexed(list) { index, entry ->
             Card(
                 shape = RoundedCornerShape(20.dp),
                 colors = CardDefaults.cardColors(
@@ -65,16 +66,17 @@ fun InterActiveScreen() {
                 Row {
                     val context = LocalContext.current
                     Checkbox(
-                        checked = entry.isSelected,
+                        checked = checkedBoxes[index].value,
                         onCheckedChange = { checked ->
                             Toast.makeText(context, "value is $checked", Toast.LENGTH_SHORT).show()
                             if (checked) { //uncheck the rest.
-                                for (item in list) {
-                                    item.FUSelected(false)
+                                for (i in 0 until list.size) {
+                                    checkedBoxes[i].value = false
+                                    list[i].FUSelected(false)
                                 }
                             }
-                           //list[entry.id].FUSelected(checked)
                             list[index].FUSelected(checked)
+                            checkedBoxes[index].value = list[index].isSelected
                         },
                     )
                     Text(
