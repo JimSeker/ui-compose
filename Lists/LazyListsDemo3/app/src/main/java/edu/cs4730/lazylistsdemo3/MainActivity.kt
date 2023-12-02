f5f3df7a00dc2e90979e440b83c84266737728b6
Jim Ward <JimSeker@users.noreply.github.com>
2023-12-02 14:44:37 -0700

    added tabs to the pager.

--- Lists/LazyListsDemo3/app/src/main/java/edu/cs4730/lazylistsdemo3/MainActivity.kt
+++ Lists/LazyListsDemo3/app/src/main/java/edu/cs4730/lazylistsdemo3/MainActivity.kt
@@ -6,22 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import edu.cs4730.lazylistsdemo3.ui.theme.LazyListsDemo3Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,12 +77,34 @@ fun MainScreen() {
             TopBar()
         },
     ) { padding ->
-        val pagerState = rememberPagerState(pageCount = { 4 })
+        var pagerState = rememberPagerState(pageCount = { 4 })
+        var tabIndex by remember { mutableStateOf(0) }
+        val coroutineScope = rememberCoroutineScope()
+        LaunchedEffect(pagerState) {
+            // When the pager changes, we also need to change the tab correctly.
+            snapshotFlow { pagerState.currentPage }.collect { page ->
+                tabIndex = page
+            }
+        }
 
+        val tabs = listOf("Simple", "simple2", "Interactive list", "PhoneBook list")
         Column(
-            modifier = Modifier.padding(padding),
+            modifier = Modifier.padding(padding).fillMaxWidth(),
         ) {
-            //Text("Page demo")
+            ScrollableTabRow(selectedTabIndex = tabIndex) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = tabIndex == index,
+                        onClick = { tabIndex = index
+                            coroutineScope.launch {
+                                // Call scroll to on pagerState, when the tab is clicked.
+                                pagerState.scrollToPage(index)
+                            }
+                        },
+                    )
+                }
+            }
             //VerticalPager for vertical paging, otherwise the same.
             HorizontalPager(state = pagerState) { page ->
                 when (page) {
